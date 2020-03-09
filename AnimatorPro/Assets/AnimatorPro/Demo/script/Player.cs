90fd99ea0a35fca9956f7dc79c476b44a3667a0a
bnm000215 <53283888+bnm000215@users.noreply.github.com>
2020-03-09 23:21:53 +0900

    1.0.0
    유니티 애니메이터에 파라미터에 대하여 좀더 어썸하게 사용할 수 있도록 고안된 Asset입니다.

---
+++ AnimatorPro/Assets/AnimatorPro/Demo/script/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.AnimatorPro;
+
+public class Player : MonoBehaviour
+{
+    private AnimatorPro AnimatorPro;
+    public Animator anim;
+
+    //해쉬를 사용하는 방법이 효율이 좋습니다.
+    private static readonly int ID_Move = Animator.StringToHash("Move");
+    
+    private void Awake()
+    {
+        AnimatorPro = new AnimatorPro();
+
+        //사용할 애니메이션을 할당해주세요.
+        AnimatorPro.Init(anim);
+    }
+
+    private void Update()
+    {
+        //이동 애니메이션 재생
+        var xx = Input.GetAxisRaw("Horizontal");
+        AnimatorPro.SetParam(ID_Move, Mathf.Abs(xx));
+        
+        //공격 애니메이션 재생
+        if (Input.GetKeyDown(KeyCode.Space))
+            AnimatorPro.SetTrigger("Attack");
+        
+        #region Other - Horizontal Flip
+        if (xx != 0.0f)
+            transform.localScale = new Vector3(-xx, 1, 1);
+        #endregion
+    }
+}
---
+++ AnimatorPro/Assets/AnimatorPro/Script/AnimatorPro.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace UnityEngine
+{
+    namespace AnimatorPro
+    {
+        [Serializable]
+        public class AnimatorPro
+        {
+            private Animator Animator;
+
+            public void Init(Animator anim) =>
+                Animator = anim;
+
+            public void SetTrigger(string paramName) =>
+                Animator?.SetTrigger(paramName);
+
+            public void SetTrigger(int paramID) =>
+                Animator?.SetTrigger(paramID);
+
+            public void SetParam<T>(string paramName, T value)
+            {
+                switch (typeof(T).Name)
+                {
+                    case "Int32":
+                        Animator?.SetInteger(paramName, Convert.ToInt32(value));
+                        break;
+
+                    case "Single":
+                        Animator?.SetFloat(paramName, Convert.ToSingle(value));
+                        break;
+
+                    case "Boolean":
+                        Animator?.SetBool(paramName, Convert.ToBoolean(value));
+                        break;
+
+                    default:
+                        throw new Exception($"The type {typeof(T)} is a type that cannot be used with SetParam.");
+                }
+            }
+
+            public void SetParam<T>(int paramID, T value)
+            {
+                switch (typeof(T).Name)
+                {
+                    case "Int32":
+                        Animator?.SetInteger(paramID, Convert.ToInt32(value));
+                        break;
+
+                    case "Single":
+                        Animator?.SetFloat(paramID, Convert.ToSingle(value));
+                        break;
+
+                    case "Boolean":
+                        Animator?.SetBool(paramID, Convert.ToBoolean(value));
+                        break;
+
+                    default:
+                        throw new Exception($"The type {typeof(T)} is a type that cannot be used with SetParam.");
+                }
+            }
+
+            public T GetParam<T>(string paramName) where T : struct, IConvertible
+            {
+                switch (typeof(T).Name)
+                {
+                    case "Int32":
+                        return (T) Convert.ChangeType(Animator?.GetInteger(paramName), typeof(T));
+
+                    case "Single":
+                        return (T) Convert.ChangeType(Animator?.GetFloat(paramName), typeof(T));
+
+                    case "Boolean":
+                        return (T) Convert.ChangeType(Animator?.GetBool(paramName), typeof(T));
+
+                    default:
+                        throw new Exception($"The type {typeof(T)} is a type that cannot be used with GetParam.");
+                }
+            }
+
+            public T GetParam<T>(int paramID) where T : struct, IConvertible
+            {
+                switch (typeof(T).Name)
+                {
+                    case "Int32":
+                        return (T) Convert.ChangeType(Animator?.GetInteger(paramID), typeof(T));
+
+                    case "Single":
+                        return (T) Convert.ChangeType(Animator?.GetFloat(paramID), typeof(T));
+
+                    case "Boolean":
+                        return (T) Convert.ChangeType(Animator?.GetBool(paramID), typeof(T));
+
+                    default:
+                        throw new Exception($"The type {typeof(T)} is a type that cannot be used with GetParam.");
+                }
+            }
+        }
+    }
+}
