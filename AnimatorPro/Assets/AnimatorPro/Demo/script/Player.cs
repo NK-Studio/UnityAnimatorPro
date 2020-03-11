976aad6805c4cf56368a432459cb7699a40156fa
bnm000215 <53283888+bnm000215@users.noreply.github.com>
2020-03-11 16:43:30 +0900

    애니메이션 상태에 대한 애트리뷰트를 통해 함수를 실행시킬 수 있습니다.

--- AnimatorPro/Assets/AnimatorPro/Demo/script/Player.cs
+++ AnimatorPro/Assets/AnimatorPro/Demo/script/Player.cs
@@ -1,35 +1,51 @@
 ﻿using UnityEngine;
 using UnityEngine.AnimatorPro;
 
+[RequireComponent(typeof(AnimatorPro))]
 public class Player : MonoBehaviour
 {
-    private AnimatorPro AnimatorPro;
+    private AnimatorPro animatorPro;
     public Animator anim;
-
+    
     //해쉬를 사용하는 방법이 효율이 좋습니다.
     private static readonly int ID_Move = Animator.StringToHash("Move");
-    
+
     private void Awake()
     {
-        AnimatorPro = new AnimatorPro();
+        //컴포넌트 할당.
+        animatorPro = GetComponent<AnimatorPro>();
 
         //사용할 애니메이션을 할당해주세요.
-        AnimatorPro.Init(anim);
+        animatorPro.Init(anim);
     }
-
+    
     private void Update()
     {
         //이동 애니메이션 재생
         var xx = Input.GetAxisRaw("Horizontal");
-        AnimatorPro.SetParam(ID_Move, Mathf.Abs(xx));
-        
+        animatorPro.SetParam(ID_Move, Mathf.Abs(xx));
+
         //공격 애니메이션 재생
         if (Input.GetKeyDown(KeyCode.Space))
-            AnimatorPro.SetTrigger("Attack");
+            animatorPro.SetTrigger("Attack");
         
         #region Other - Horizontal Flip
+
         if (xx != 0.0f)
             transform.localScale = new Vector3(-xx, 1, 1);
+
         #endregion
     }
+
+    [AnimatorEnter("Base.Attack")]
+    public void AttackAnimEnter() =>
+        Debug.Log("공격을 Enter");
+    
+    [AnimatorStay("Base.Attack")]
+    public void AttackAnimStay() =>
+        Debug.Log("공격을 Stay");
+    
+    [AnimatorExit("Base.Attack")]
+    public void AttackAnimExit() =>
+        Debug.Log("공격을 Exit");
 }
--- AnimatorPro/Assets/AnimatorPro/Script/AnimatorPro.cs
+++ AnimatorPro/Assets/AnimatorPro/Script/AnimatorPro.cs
@@ -1,16 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
+// 해당 클래스 사용시 [RequireComponent(typeof(AnimatorPro))] 애트리뷰트를 사용하시는 클래스 위에 추가해주세요.
+// When using the class, add the [RequireComponent(typeof(AnimatorPro))] attribute on the class.
 namespace UnityEngine
 {
     namespace AnimatorPro
     {
-        [Serializable]
-        public class AnimatorPro
+        [ExecuteInEditMode]
+        [DisallowMultipleComponent]
+        public class AnimatorPro : MonoBehaviour
         {
             private Animator Animator;
 
-            public void Init(Animator anim) =>
+            public void Init(Animator anim)
+            {
                 Animator = anim;
+
+                #region StatePro
+
+                _lastStateLayers = new int[Animator.layerCount];
+                DiscoverStateMethods();
+
+                #endregion
+            }
+
+             private void Awake() => // Make the component invisible in the inspector.
+                 hideFlags = HideFlags.HideInInspector;
+
+            #region ParameterPro
 
             public void SetTrigger(string paramName) =>
                 Animator?.SetTrigger(paramName);
@@ -95,6 +115,143 @@ namespace UnityEngine
                         throw new Exception($"The type {typeof(T)} is a type that cannot be used with GetParam.");
                 }
             }
+
+            #endregion
+
+            #region StatePro
+
+            private Lookup<int, Action> stateHashToUpdateMethod;
+            private Lookup<int, Action> stateHashToEnterMethod;
+            private Lookup<int, Action> stateHashToExitMethod;
+            private Dictionary<int, string> hashToAnimString;
+            private int[] _lastStateLayers;
+
+            private void Update() =>
+                StateMachineUpdate();
+
+            private void StateMachineUpdate()
+            {
+                for (var layer = 0; layer < _lastStateLayers.Length; layer++)
+                {
+                    var _lastState = _lastStateLayers[layer];
+                    var stateId = Animator.GetCurrentAnimatorStateInfo(layer).fullPathHash;
+                    if (_lastState != stateId)
+                    {
+                        if (stateHashToExitMethod.Contains(_lastState))
+                            foreach (var action in stateHashToExitMethod[_lastState])
+                                action.Invoke();
+
+                        if (stateHashToEnterMethod.Contains(stateId))
+                            foreach (var action in stateHashToEnterMethod[stateId])
+                                action.Invoke();
+                    }
+
+                    if (stateHashToUpdateMethod.Contains(stateId))
+                        foreach (var action in stateHashToUpdateMethod[stateId])
+                            action.Invoke();
+
+                    _lastStateLayers[layer] = stateId;
+                }
+            }
+
+            private void DiscoverStateMethods()
+            {
+                hashToAnimString = new Dictionary<int, string>();
+                var components = gameObject.GetComponents<MonoBehaviour>();
+
+                var enterStateMethods = new List<StateMethod>();
+                var updateStateMethods = new List<StateMethod>();
+                var exitStateMethods = new List<StateMethod>();
+
+                foreach (var component in components)
+                {
+                    if (component == null) continue;
+
+                    var type = component.GetType();
+                    var methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public |
+                                                  BindingFlags.DeclaredOnly |
+                                                  BindingFlags.InvokeMethod);
+
+                    foreach (var method in methods)
+                    {
+                        var attributes = method.GetCustomAttributes(typeof(AnimatorStay), true);
+                        updateStateMethods.AddRange(from AnimatorStay attribute in attributes
+                            let parameters = method.GetParameters()
+                            where parameters.Length == 0
+                            select CreateStateMethod(attribute.state, method, component));
+
+
+                        attributes = method.GetCustomAttributes(typeof(AnimatorEnter), true);
+                        enterStateMethods.AddRange(from AnimatorEnter attribute in attributes
+                            let parameters = method.GetParameters()
+                            where parameters.Length == 0
+                            select CreateStateMethod(attribute.state, method, component));
+
+                        attributes = method.GetCustomAttributes(typeof(AnimatorExit), true);
+                        exitStateMethods.AddRange(from AnimatorExit attribute in attributes
+                            let parameters = method.GetParameters()
+                            where parameters.Length == 0
+                            select CreateStateMethod(attribute.state, method, component));
+                    }
+                }
+
+                stateHashToUpdateMethod =
+                    (Lookup<int, Action>) updateStateMethods.ToLookup(p => p.stateHash, p => p.method);
+                stateHashToEnterMethod =
+                    (Lookup<int, Action>) enterStateMethods.ToLookup(p => p.stateHash, p => p.method);
+                stateHashToExitMethod =
+                    (Lookup<int, Action>) exitStateMethods.ToLookup(p => p.stateHash, p => p.method);
+            }
+
+            private StateMethod CreateStateMethod(string state, MethodBase method, MonoBehaviour component)
+            {
+                var stateHash = Animator.StringToHash(state);
+                hashToAnimString[stateHash] = state;
+                var stateMethod = new StateMethod
+                {
+                    stateHash = stateHash, method = () => { method.Invoke(component, null); }
+                };
+                return stateMethod;
+            }
+
+            #endregion
         }
+
+        #region StatePro
+
+        [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+        public class AnimatorStay : Attribute
+        {
+            public readonly string state;
+
+            public AnimatorStay(string state) =>
+                this.state = state;
+        }
+
+        [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+        public class AnimatorEnter : Attribute
+        {
+            public readonly string state;
+
+            public AnimatorEnter(string state) =>
+                this.state = state;
+        }
+
+        [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+        public class AnimatorExit : Attribute
+        {
+            public readonly string state;
+
+            public AnimatorExit(string state) =>
+                this.state = state;
+        }
+
+        public class StateMethod
+        {
+            public int stateHash;
+            public Action method;
+        }
+
+        #endregion
     }
 }
